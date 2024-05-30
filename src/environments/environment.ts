6066a03e4bc33ae85313b5ea295807451494a112
rgueta <ricardogueta@hotmail.com>
2024-05-30 15:32:16 -0700

    added mode ios on few pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,8 +11,8 @@ export const environment = {
     debugging_send_sms: false
   },
   cloud : {
-    // server_url : "http://100.24.58.74/"
-    server_url : "http://192.168.1.89:5500/"
+    server_url : "http://100.24.58.74/"
+    // server_url : "http://192.168.1.89:5500/"
 
     // socket_url : "ws://192.168.1.154:5500/",
   }
