849c1a94a43dadf11a95359d706a515f7cd8d0ea
rgueta <ricardogueta@hotmail.com>
2024-10-04 05:49:40 -0700

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   app : {
-    version: "1.0.0",
+    version: "1.0.1",
     Description: "Main tab data information feed",
     debugging: false,
     debugging_send_sms: false
